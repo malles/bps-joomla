94d6d1b1d82d486179811abba43bc7207b3b7b22
malles <plop0999>
2014-09-30 15:21:14 +0200

    2.5.25

--- components/com_users/views/registration/tmpl/complete.php
+++ components/com_users/views/registration/tmpl/complete.php
@@ -1,14 +1,18 @@
 <?php
 /**
-* @package   yoo_solar
-* @author    YOOtheme http://www.yootheme.com
-* @copyright Copyright (C) YOOtheme GmbH
-* @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
-*/
-?>
-<div id="system">
-	<h1 class="title">ACCOUNT AANGEVRAAGD</h1>
-	<p>Bedankt voor het aanvragen van het account.</p>
+ * @package		Joomla.Site
+ * @subpackage	com_users
+ * @copyright	Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ * @since		1.6
+ */
 
-    <a href="/" class="button-default">Homepage</a>
+defined('_JEXEC') or die;
+?>
+<div class="registration-complete<?php echo $this->pageclass_sfx;?>">
+	<?php if ($this->params->get('show_page_heading')) : ?>
+	<h1>
+		<?php echo $this->escape($this->params->get('page_heading')); ?>
+	</h1>
+	<?php endif; ?>
 </div>
